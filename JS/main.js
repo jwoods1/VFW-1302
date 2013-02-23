050b558690e093a9db7dac032e32cf4fc0b7ee92
b87775 <jwoods1@fullsail.edu>
2013-02-23 13:19:24 -0700

    Added edit and delete function.  Debugged edit function.  Added add button to top of display page.

--- JS/main.js
+++ JS/main.js
@@ -27,7 +27,9 @@ window.addEventListener("DOMContentLoaded", function(){
 
 		}
 		selectLi.appendChild(makeSelect);
-	};
+	}
+
+	
 
 	function getAutoV(){
 		if($("auto").checked){
@@ -35,7 +37,7 @@ window.addEventListener("DOMContentLoaded", function(){
 		}else{
 			autoValue = "No"
 		}
-	};
+	}
 
 	function getMotoV(){
 		if($("motorcycle").checked){
@@ -43,7 +45,7 @@ window.addEventListener("DOMContentLoaded", function(){
 		}else{
 			motoValue = "No"
 		}
-	};
+	}
 
 	function getRvV(){
 		if($("rv").checked){
@@ -51,7 +53,7 @@ window.addEventListener("DOMContentLoaded", function(){
 		}else{
 			rvValue = "No"
 		}
-	};
+	}
 
 	function getHomeV(){
 		if($("home").checked){
@@ -59,7 +61,7 @@ window.addEventListener("DOMContentLoaded", function(){
 		}else{
 			homeValue = "No"
 		}
-	};
+	}
 
 	function getRentV(){
 		if($("rent").checked){
@@ -67,7 +69,7 @@ window.addEventListener("DOMContentLoaded", function(){
 		}else{
 			rentValue = "No"
 		}
-	};
+	}
 
 	function getLifeV(){
 		if($("life").checked){
@@ -75,9 +77,36 @@ window.addEventListener("DOMContentLoaded", function(){
 		}else{
 			lifeValue = "No"
 		}
-	};
-	function storeData(){
-		var id      = Math.floor(Math.random()*100000001);
+	}
+	
+	function toggleControls(n){
+		switch(n){
+			case "on":
+				$("NewClient").style.display = "none";
+				$("clear").style.display = "inline";
+				$("add").style.display = "inline";
+				$("DisplayData").style.display = "none";
+				$("myButton").style.display = "inline";
+				break;
+			case "off":
+				$("NewClient").style.display = "block";
+				$("clear").style.display = "inline";
+				$("add").style.display = "none";
+				$("DisplayData").style.display = "inline";
+				$("myButton").style.display = "inline";
+				$("items").style.display = "none";
+				break;
+			default:
+				return false;
+		}
+	}
+
+	function storeData(key){
+		if(!key){
+			var id 			= Math.floor(Math.random()*100000001);
+		}else{
+			id = key;
+		}
 		getAutoV();
 		getMotoV();
 		getRvV();
@@ -100,35 +129,13 @@ window.addEventListener("DOMContentLoaded", function(){
 			item.life		= ["Life Insurance:", lifeValue];
 			item.numV		= ["Number of Vehicles:", $("AutoNum").value];
 			item.numPoliy   = ["Policy Number:", $("Pnum").value];
-			item.comment	= ["Additoional Comments:", $("Comments").value];
+			item.comment	= ["Additional Comments:", $("Comments").value];
 			item.nDate		= ["Renewal Date:", $("renew").value];
 
 		localStorage.setItem(id, JSON.stringify(item));
 		alert("Contact Saved");
 
-	};
-	function toggleControls(n){
-		switch(n){
-			case "on":
-				$("NewClient").style.display = "none";
-				$("clear").style.display = "inline";
-				$("add").style.display = "inline";
-				$("DisplayData").style.display = "none";
-				$("myButton").style.display = "inline";
-				break;
-			case "off":
-				$("NewClient").style.display = "block";
-				$("clear").style.display = "inline";
-				$("add").style.display = "none";
-				$("DisplayData").style.display = "inline";
-				$("myButton").style.display = "none";
-				$("items").style.display = "none";
-				break;
-			default:
-				return false;
-		}
 	}
-
 //Get local Storage function
 
 	function getLocal(){
@@ -147,9 +154,9 @@ window.addEventListener("DOMContentLoaded", function(){
 			var linkLi = document.createElement("li");
 			linkLi.setAttribute("id","links");
 			makeList.appendChild(makeli);
-			var Key = localStorage.key(i);
-			var value = localStorage.getItem(Key);
-			//convert the string from local storage value back to an Object by useing JSON.parse()
+			var key = localStorage.key(i);
+			var value = localStorage.getItem(key);
+			//convert the string from local storage value back to an Object by using JSON.parse()
 			var lObj = JSON.parse(value);
 			var createSubList = document.createElement("ul");
 			makeli.appendChild(createSubList);
@@ -160,38 +167,82 @@ window.addEventListener("DOMContentLoaded", function(){
 				createSubli.innerHTML = optSubText;
 				createSubList.appendChild(linkLi);
 			}
-			makeItemLink(localStorage.key(i), linkLi);//create our edit and delete buttons. for each item in local storage.
+			makeItemLink(key, linkLi);//create our edit and delete buttons. for each item in local storage.
 		}
 
 	}
+	
+	
 	//create the edit and delete links for each stored item
 	function makeItemLink(key, linkLi){
-		var editlink = document.createElement("a");
-		editlink.href = "#";
-		editlink.key = key;// Check CAPITAL
+		var editLink = document.createElement("a");
+		editLink.href = "#";
+		editLink.key = key;// Check CAPITAL
 		var editText = "Edit Client";
-		//editlink.addEventListener("click", editItem);
-		editlink.innerHTML = editText;
-		linkLi.appendChild(editlink);
-
-		var addClient = document.createElement("a");
-		addClient.href ="additem.html";
-		addClient.key = key;
-		var addText = "Add New Client";
-		addClient.innerHTML = addText;
-		linkLi.appendChild(addClient);
-
+		editLink.addEventListener("click", editItem);
+		editLink.innerHTML = editText;
+		linkLi.appendChild(editLink);
 
 		var deleteLink = document.createElement("a");
 		deleteLink.href = "#";
 		deleteLink.key = key; // CHECK CAPITAL
 		var deleteText = "Delete Client";
-		//deleteLink.addEventListener("click", deleteItem);
+		deleteLink.addEventListener("click", deleteItem);
 		deleteLink.innerHTML = deleteText;
 		linkLi.appendChild(deleteLink);
 
 	}
+	function deleteItem(){
+		var ask = confirm("Are you sure you want to delete this client?");
+		if(ask){
+			localStorage.removeItem(this.key);
+			window.location.reload();
+		}else{
+			alert("Contact was Not Deleted.");
+		}
+	}
+	function editItem(){
+		toggleControls("off");
+		//grab the data from our item from local storage.
+		var itValue = localStorage.getItem(this.key);
+		var item = JSON.parse(itValue);
+		//pop form
+		$("theCompany").value 	= item.company[1];
+		$("Name").value 		= item.fname[1];
+		$("Lname").value 		= item.lname[1];
+		$("email").value 		= item.email[1];
+		if(item.Auto[1] == "yes"){
+			$("auto").setAttribute("checked", "checked");
+		}
+		if(item.Moto[1] == "yes"){
+			$("motorcycle").setAttribute("checked", "checked");
+		}
+		if(item.rv[1] == "yes"){
+			$("rv").setAttribute("checked", "checked");
+		}
+		if(item.home[1] == "yes"){
+			$("home").setAttribute("checked", "checked");
+		}
+		if(item.rent[1] == "yes"){
+			$("rent").setAttribute("checked", "checked");
+		}
+		if(item.life[1] == "yes"){
+			$("life").setAttribute("checked", "checked");
+		}
+		$("AutoNum").value 		= item.numV[1];
+		$("Pnum").value 		= item.numPoliy[1];
+		$("Comments").value 	= item.comment[1];
+		$("renew").value 		= item.nDate[1];
 
+		//remove the listener from save contact button
+		saveData.removeEventListener("click", storeData);
+		//change saveData button
+		$("myButton").value = "Edit Client";
+		var editClient = $("myButton");
+		//save the key value established in the function as a property of the edit client event
+		editClient.addEventListener("click", validate);
+		editClient.key = this.key;
+	}
 
 	function clearLocal(){
 		if(localStorage.length === 0){
@@ -205,22 +256,97 @@ window.addEventListener("DOMContentLoaded", function(){
 			return false;
 		}
 	}
+	function validate(e){
+		//define the elements we want to check
+		var getCompany = $("theCompany");
+		var getFname = $("Name");
+		var getLname = $("Lname");
+		var getEmail = $("email");
+		var getPnum = $("Pnum");
+		var getRnew = $("renew");
 
-	//add new client reload
+		//rest Error
+		errM.innerHTML = "";
+		getFname.style.border = "1px solid black";
+		getLname.style.border = "1px solid black";
+		getEmail.style.border = "1px solid black";
+		getCompany.style.border = "1px solid black";
+		getPnum.style.border = "1px solid black";
+		getRnew.style.border = "1px solid black";
+		// get Error messages
+		var messageA =[];
+		
+		//first name validation
+		if(getFname.value == ""){
+			var fNameError = "Please enter a first name.";
+			getFname.style.border = "1px solid red";
+			messageA.push(fNameError);
+		}
+		//last name validation
+		if(getLname.value == ""){
+			var LNameError = "Please enter a last name.";
+			getLname.style.border = "1px solid red";
+			messageA.push(LNameError);
+		}	
+		//email validation
+		var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+		if(!(re.exec(getEmail.value))){
+			var emailError = "Please enter a valid email address."
+			getEmail.style.border = "1px solid red";
+			messageA.push(emailError);
+		}
+		//company validation
+		if(getCompany.value == "--Select Company--"){
+			var companyError = "Please select a company";
+			getCompany.style.border = "1px solid red";
+			messageA.push(companyError);
+		}
+		if(getPnum.value == ""){
+			var pNumError = "Please input a policy number";
+			getPnum.style.border = "1px solid red";
+			messageA.push(pNumError);
+		}
+		if(getRnew.value == ""){
+			var reNewError = "Please select valid renewal date.";
+			getRnew.style.border = "1px solid red";
+			messageA.push(reNewError);
+		}
 
-	function addNc(){
-		window.location.reload();
+		//if there were errors. display
+		if(messageA.length>= 1){
+			for(var i=0, j=messageA.length; i < j; i++){
+				var txt = document.createElement("li");
+				txt.innerHTML = messageA[i];
+				errM.appendChild(txt);
+			}
+			e.preventDefault();
+			return false;	
+		}else{
+			//if valid save
+			storeData(this.key);
+		}
+			
 	}
 
+	function addNewClient(){
+		var add = confirm("Do you want to ADD a new Client?");
+		if(add){
+			window.location.reload("additem.html");
+
+		}
+		
+	}
+	//variables
+
 	var companies = ["--Select Company--", "American Mod", "Farmers", "State Farm", "Progressive","All State", "Nation Wide"];
-	makeComps();
+	makeComps(), errM = $("error");
 	//set link
 	var displayDataLink = $("DisplayData");
 	displayDataLink.addEventListener("click", getLocal);
 	var clearDataLink = $("clear");
 	clearDataLink.addEventListener("click", clearLocal);
-	var addNewClient = $("add");
-	addNewClient.addEventListener("click", addNc);
+	var addClientLink = $("add");
+	addClientLink.addEventListener("click", addNewClient);
 	var saveData = $("myButton");
-	saveData.addEventListener("click", storeData);
+	saveData.addEventListener("click", validate);
 });
