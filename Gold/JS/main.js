b51fb21b90ec04329a815a7c8392dc1943a74a3a
b87775 <jwoods1@fullsail.edu>
2013-03-13 23:50:14 -0600

    Added Jquerymobile base templete to bronze app

--- Gold/JS/main.js
+++ Gold/JS/main.js
@@ -445,6 +445,8 @@ window.addEventListener("DOMContentLoaded", function(){
 	//set link
 	var displayDataLink = cV("DisplayData");
 	displayDataLink.addEventListener("click", getLocal);
+	var displayLink = cV("DisplayClients");
+	displayLink.addEventListener("click", getLocal);
 	var clearDataLink = cV("clear");
 	clearDataLink.addEventListener("click", clearLocal);
 	var addClientLink = cV("add");
